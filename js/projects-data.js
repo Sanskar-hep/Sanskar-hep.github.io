e542d31c69193b7a992d28885c31abc124a56e6c
Sanskar-hep <sanskarnanda04@gmail.com>
2026-08-09 15:54:31 +0530

    Made the Projects clickable

---
+++ js/projects-data.js
@@ -0,0 +1,33 @@
+var CUSTOM_PROJECTS = [
+  {
+    name: "TOP ASYMMETRY ANALYSIS",
+    summary: "Extracting flavour-separated charge asymmetries in tt̄ production using CMS Run 2 data.",
+    readmeRepo: "Sanskar-hep/TOP-ANALYSIS-FRAMEWORK",
+    url: "https://github.com/Sanskar-hep/TOP-ANALYSIS-FRAMEWORK",
+    tags: ["Python", "C++", "Shell", "ROOT"]
+  },
+
+  {
+    name: "WEB-DEVELOPMENT-TUTORIALS",
+    summary: "A collection of web development tutorials and projects.",
+    readmeRepo: "Sanskar-hep/Web-Development-Tutorials",
+    url: "https://github.com/Sanskar-hep/Web-Development-Tutorials",
+    tags: ["HTML"]
+  },
+
+  {
+    name: "Sanskar-hep.github.io",
+    summary: "My personal website built using HTML, CSS, and JavaScript.",
+    readmeRepo: "Sanskar-hep/Sanskar-hep.github.io",
+    url: "https://github.com/Sanskar-hep/Sanskar-hep.github.io",
+    tags: ["HTML", "CSS", "JavaScript"]
+  },
+
+  {
+    name: "MLSCHOOL_2026_TIFR",
+    summary: "A collection of Jupyter notebooks for the TIFR ML School 2026.",
+    readmeRepo: "Sanskar-hep/MLSCHOOL_2026_TIFR",
+    url: "https://github.com/Sanskar-hep/MLSCHOOL_2026_TIFR",
+    tags: ["Python", "Jupyter Notebook", "Machine Learning"]
+  }
+];
